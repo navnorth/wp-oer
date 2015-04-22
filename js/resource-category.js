493ddec567495165e61974b15345d05c583c5c22
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2015-04-22 17:29:08 +0800

    Updated resource category page including javascript and stylesheet

--- js/resource-category.js
+++ js/resource-category.js
@@ -5,8 +5,23 @@ jQuery(document).ready(function(){
     }
 });
 
+/** Toggle Sub Categories **/
 function toggleparent(ref)
 {
 	jQuery(ref).parent(".sub-category").toggleClass("activelist");
 	jQuery(ref).next(".resource_category").slideToggle();
+}
+
+/** Slide Toggole in Subject Button **/
+function tglcategories(ref)
+{
+	if(jQuery(ref).hasClass("open"))
+	{
+		jQuery(ref).removeClass("open")
+	}
+	else
+	{
+		jQuery(ref).addClass("open")
+	}
+    jQuery(".resource_category_sidebar").slideToggle("slow");
 }
