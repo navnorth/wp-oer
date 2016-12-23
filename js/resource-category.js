2aa224eac37b450b94f6ecd8be217565687f66a4
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2016-12-24 00:26:29 +0800

    more debugging to fix height

--- js/resource-category.js
+++ js/resource-category.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function(){
     }
     
     //Sets height of browse section to 4 items by default
-    var bHeight = jQuery('.oer-snglrsrc').outerHeight(true);
+    var bHeight = jQuery('.oer-snglrsrc:first-child').outerHeight();
     console.log(bHeight);
     jQuery('.oer-allftrdrsrccntr').css('height',bHeight*4+'px')
 });
