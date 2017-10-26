10af3b435a9d37068fbd52f6f410d5bf417b5d57
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2017-10-27 00:49:38 +0800

    adjust search default template for different themes

--- oer_template/search.php
+++ oer_template/search.php
@@ -27,7 +27,7 @@ get_header(); ?>
 				 * If you want to overload this in a child theme then include a file
 				 * called content-search.php and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', 'search' );
+				load_template(OER_PATH.'oer_template/search-content.php');
 
 			// End the loop.
 			endwhile;
