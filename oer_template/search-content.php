098608e8bcaab7fede7e36f651d2221f5f639b17
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2017-10-30 23:59:05 +0800

    update edit link for default WP themes

--- oer_template/search-content.php
+++ oer_template/search-content.php
@@ -21,12 +21,12 @@
 			<div class="entry-meta">
 				<?php
 				echo twentyseventeen_time_link();
-				twentyseventeen_edit_link();
+				edit_link();
 				?>
 			</div><!-- .entry-meta -->
 		<?php elseif ( 'page' === get_post_type() && get_edit_post_link() ) : ?>
 			<div class="entry-meta">
-				<?php twentyseventeen_edit_link(); ?>
+				<?php edit_link(); ?>
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
 
