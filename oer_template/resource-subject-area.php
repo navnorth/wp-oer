b6102b27cb42747d03b27b600f48c05f82c7ad86
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2017-11-07 23:40:41 +0800

    add meta data to subject area pages

--- oer_template/resource-subject-area.php
+++ oer_template/resource-subject-area.php
@@ -428,6 +428,7 @@ $hide_title = get_option('oer_hide_subject_area_title');
 								}
 							}
 						}
+			
 					?>
 						<div class="oer-snglrsrc">
 							<?php
@@ -435,7 +436,105 @@ $hide_title = get_option('oer_hide_subject_area_title');
 							?>
 							<div class="oer-snglttldscrght <?php if(empty($img_url)){ echo 'snglttldscrghtfull';}?>">
 								<div class="ttl"><a href="<?php echo esc_url(get_permalink($post->ID));?>"><?php echo $title;?></a></div>
+							<?php
+								$subjects = array();
+								$grades = array();
+								//Display Meta of Resource
+								if ($post->post_type=="resource"){
+									$ID = $post->ID;
+									$url = get_post_meta($ID, "oer_resourceurl", true);
+									$url_domain = oer_getDomainFromUrl($url);
+									$grades =  trim(get_post_meta($ID, "oer_grade", true),",");
+									?>
+									<div class="post-meta">
+									<?php
+									if ($grades) {
+										$grades = explode(",",$grades);
+										if(is_array($grades) && !empty($grades) && array_filter($grades)){
+											$grade_label = "Grade: ";
+											if (count($grades)>1)
+												$grade_label = "Grades: ";
+											
+											echo "<span class='post-meta-box post-meta-grades'><strong>".$grade_label."</strong>";
+											
+											sort($grades);
+					
+											for($x=0; $x < count($grades); $x++)
+											{
+											  $grades[$x];
+											}
+											$fltrarr = array_filter($grades, 'strlen');
+											$flag = array();
+											$elmnt = $fltrarr[min(array_keys($fltrarr))];
+											for($i =0; $i < count($fltrarr); $i++)
+											{
+												if($elmnt == $fltrarr[$i] || "k" == strtolower($fltrarr[$i]))
+												{
+													$flag[] = 1;
+												}
+												else
+												{
+													$flag[] = 0;
+												}
+												$elmnt++;
+											}
+					
+											if(in_array('0',$flag))
+											{
+												echo implode(",",array_unique($fltrarr));
+											}
+											else
+											{
+												$arr_flt = array_keys($fltrarr);
+												$end_filter = end($arr_flt);
+												if (count($fltrarr)>1) {
+													if (strtolower($fltrarr[$end_filter])=="k") {
+														$last_index = count($fltrarr)-2;
+														echo $fltrarr[$end_filter]."-".$fltrarr[$last_index];
+													}
+													else
+														echo $fltrarr[0]."-".$fltrarr[$end_filter];
+												}
+												else
+													echo $fltrarr[0];
+											}
+											echo "</span>";
+										}
+									}
+									if ($url_domain) {
+										?>
+										<span class="post-meta-box post-meta-domain"><strong>Domain: </strong><a href="<?php echo esc_url(get_post_meta($ID, "oer_resourceurl", true)); ?>" target="_blank" >
+										<?php echo $url_domain; ?>
+										</a></span>
+										<?php
+									}
+									?>
+									</div>
+									<?php
+									$subjects = oer_get_subject_areas($ID);
+								}
+								?>
 								<div class="desc"><?php echo $content; ?></div>
+								<?php
+								// Display subject areas
+								if ($subjects) {
+									$oer_subjects = array_unique($subjects, SORT_REGULAR);
+						
+									if(!empty($oer_subjects))
+									{
+										?>
+										<div class="tagcloud">
+										<?php
+										foreach($oer_subjects as $subject)
+										{
+											echo '<span><a href="'.esc_url(site_url().'/'.$subject->taxonomy.'/'.$subject->slug).'" class="button">'.ucwords ($subject->name).'</a></span>';
+										}
+										?>
+										</div>
+										<?php
+									}
+								}
+								?>
 							</div>
 						</div>
 					<?php
