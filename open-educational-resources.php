b1f5669c991c1bc55a4076d3791f0699f123b80c
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-03-18 20:51:17 +0800

    fix template loading

--- open-educational-resources.php
+++ open-educational-resources.php
@@ -319,21 +319,23 @@ function oer_category_template( $template ) {
 	global $wp_query;
 
 	$object = $wp_query->get_queried_object();
-	var_dump($object);
-	//Post ID
-	$_id = $wp_query->get_queried_object_id();
-
-	// Get Current Object if it belongs to Resource Category taxonomy
-	$resource_term = get_term_by( 'id' , $_id , 'resource-subject-area' );
 	
-	//Check if the loaded resource is a category
-	if ($resource_term && !is_wp_error( $resource_term )) {
-		return oer_get_template_hierarchy('resource-subject-area');
-	} else {
-		if ($wp_query->is_search)
-			return oer_get_template_hierarchy("search");
-		return $template;
+	if (is_a($object,"WP_Term")) {
+		//Post ID
+		$_id = $wp_query->get_queried_object_id();
+	
+		// Get Current Object if it belongs to Resource Category taxonomy
+		$resource_term = get_term_by( 'id' , $_id , 'resource-subject-area' );
+		
+		//Check if the loaded resource is a category
+		if ($resource_term && !is_wp_error( $resource_term )) {
+			return oer_get_template_hierarchy('resource-subject-area');
+		} else {
+			if ($wp_query->is_search)
+				return oer_get_template_hierarchy("search");
+		}
 	}
+	return $template;
  }
 
  /**
