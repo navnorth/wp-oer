01409f3e1e1b454a4927cdadf302db42e7cca74f
johnpaulbalagolan <johnpaulbalagolan@gmail.com>
2020-03-04 21:00:13 +0800

    change post_date on resource import

--- includes/oer-functions.php
+++ includes/oer-functions.php
@@ -1217,7 +1217,7 @@ function oer_importResources($default=false) {
 					/** Get Current WP User **/
 					$user_id = get_current_user_id();
 					/** Get Current Timestamp for post_date **/
-					$cs_date = current_time('mysql');
+					$cs_date = current_time('mysql', false);
 
 					$post = array('post_content' => $oer_description, 'post_name' => $post_name, 'post_title' => $oer_title, 'post_status' => 'publish', 'post_type' => 'resource', 'post_author' => $user_id , 'post_date' => $cs_date, 'post_date_gmt'  => $cs_date, 'comment_status' => 'open');
 					/** Set $wp_error to false to return 0 when error occurs **/
