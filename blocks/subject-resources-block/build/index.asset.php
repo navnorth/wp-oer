5754d52ee9b869d6f2a44fc7f531d681b3c97daf
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2020-11-29 19:28:28 +0800

    fix error when adding WP OER Subject Resources Block in page

--- blocks/subject-resources-block/build/index.asset.php
+++ blocks/subject-resources-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'afb2d1a42e51880e877c5898b62e4c5c');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '00fdaf4e6c096cf084daa9a7046b4d6c');
