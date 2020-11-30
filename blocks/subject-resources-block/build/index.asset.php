fc3cdb29c6fd1744408b5b7fa8223e2296164411
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2020-11-30 19:36:15 +0800

    fix to js error encountered on the test site

--- blocks/subject-resources-block/build/index.asset.php
+++ blocks/subject-resources-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7a437d9add4fe32483e4a8ab50dc41df');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4953d3b7c390641b37020baa3b93f4df');
