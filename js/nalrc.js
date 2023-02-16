2dc3229a359c5b88f066ca4f7682e8df573f00c9
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2023-02-16 23:21:02 +0800

    OESE-280 debug not selectable option issue

--- js/nalrc.js
+++ js/nalrc.js
@@ -100,18 +100,6 @@ jQuery(function($){
 	      }
 	      $('.nalrc-select-filter .dropdown-menu li:nth-child('+itemFocus+') a').focus();
 	    });
-
-	    // Keyboard navigation of filter dropdown on Resources page
-		$(document).on('keydown', '.nalrc-select-filter button.dropdown-toggle', function(e){
-			var code = e.keyCode || e.which;
-			if (code==13 || code==32){
-				$(this).closest('.bootstrap-select').find('.selectpicker').selectpicker('toggle');
-			} else if (code==38) {
-				$(this).closest('.bootstrap-select').find('.dropdown-menu li:last-child a').focus();
-			} else if (code==40) {
-				$(this).closest('.bootstrap-select').find('.dropdown-menu li:first-child a').focus();
-			}
-		});
 	}
 
 	/** Topic Search **/
