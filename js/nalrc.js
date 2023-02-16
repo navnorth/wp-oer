30eb17717a9d8d7847c991ee30364f564b908c24
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2023-02-16 14:08:29 +0800

    OESE-280 up and arrow navigation on filter dropdown

--- js/nalrc.js
+++ js/nalrc.js
@@ -68,6 +68,32 @@ jQuery(function($){
 		$(this).addClass('active');
 		$(this).closest('li').addClass('active');
 	});--**/
+	$('.nalrc-select-filter div.dropdown-menu.show li').on('keydown', function (e) {
+	    if (e.keyCode == 38) { // Up
+	      	var previousEle = $(this).prev();
+	      	if (previousEle.length == 0) {
+	        	previousEle = $(this).nextAll().last();
+	      	}
+		    var selVal = $('.selectpicker option').filter(function () {
+		        return $(this).text() == previousEle.text();
+		    }).val();
+		    $('.selectpicker').selectpicker('val', selVal);
+
+		     return;
+	    }
+	    if (e.keyCode == 40) { // Down
+	      	var nextEle = $(this).next();
+	      	if (nextEle.length == 0) {
+	        	nextEle = $(this).prevAll().last();
+	      	}
+	      	var selVal = $('.selectpicker option').filter(function () {
+	        	return $(this).text() == nextEle.text();
+	      	}).val();
+	      	$('.selectpicker').selectpicker('val', selVal);
+
+	      	return;
+	    }
+  });
 
 	/** Topic Search **/
 	$('.nalrc-search-button').on('click', searchResources);
