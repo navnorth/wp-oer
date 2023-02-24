af0b63a8f68ce2d32761de8528b2ec823c8fdd06
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2023-02-24 16:31:29 +0800

    OESE-302 add topic area if not existing on resource import

--- includes/oer-functions.php
+++ includes/oer-functions.php
@@ -1212,6 +1212,10 @@ function oer_importResources($default=false) {
 							    {
 								    // Categories are not found then assign as keyword
 								    $oer_kywrd .= ",".$oer_categories [$i];
+								    
+								    // add the term if category does not exist
+								    $cat = wp_insert_term($oer_categories[$i], 'resource-subject-area');
+								    $category_id[$i] = $cat['term_id'];
 							    }
 							}
 						}
