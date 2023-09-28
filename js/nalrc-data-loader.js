e84e0c48683ac96386a7ca1c9813c8e8dbdb35bd
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2023-09-28 21:30:53 +0800

    OESE-331 create nalrc data loader script for search resources

---
+++ js/nalrc-data-loader.js
@@ -0,0 +1,40 @@
+jQuery(function($){
+  	if (typeof nalrc_object != 'undefined')
+    	//nalrc_object.ajaxurl = 'https://oese.ed.gov/wp-content/plugins/wp-usahtmlmap-3.2.9/ajax.php';
+    	nalrc_object.ajaxurl = 'https://oese.wp.nnth.dev/wp-content/plugins/wp-customusahtmlmap/ajax.php';
+
+	function narlc_searchResources(){
+		$('.oer_resource_posts').html('');
+		var loader = '<div class="resource-loader"><img src="'+nalrc_object.plugin_url +'/images/load.gif" /></div>';
+		$('.oer_resource_posts').html(loader);
+		var data = {
+			action: 'search_resources',
+		}
+		if ($('.nalrc-search-filters #keyword').val()!=='')
+			data.keyword = $('.nalrc-search-filters #keyword').val();
+		if ($('.nalrc-search-filters #gradeLevel').val()!=='')
+			data.gradeLevel = $('.nalrc-search-filters #gradeLevel').val();
+		if ($('.nalrc-search-filters #product').val()!=='')
+			data.product = $('.nalrc-search-filters #product').val();
+		
+		$.ajax({
+			type: "POST",
+			url: nalrc_object.ajaxurl,
+			data: data,
+			success: function(msg){
+				$('.oer_resource_posts').html(msg);
+			}
+		});
+	}
+
+	/** Topic Search **/
+	$('.nalrc-search-button').on('click', narlc_searchResources);
+
+	/** Keyword search **/
+	$('.nalrc-search-keyword #keyword').on('keydown', function(e){
+		var code = e.keyCode || e.which;
+		if (code==13){
+			narlc_searchResources();
+		}
+	});
+});
