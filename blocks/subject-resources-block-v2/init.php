8000fbf15fc5b2a3312c73905afd3e70e855beab
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2023-09-30 22:11:53 +0800

    OESE-328 remove WP-API

--- blocks/subject-resources-block-v2/init.php
+++ blocks/subject-resources-block-v2/init.php
@@ -7,7 +7,6 @@
  * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
  */
 function wp_oer_subject_resources_block_init() {
-    wp_enqueue_script('wp-api');
     $dir = dirname(__FILE__);
     $version_58 = is_version_58();
 
